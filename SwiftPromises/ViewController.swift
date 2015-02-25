3d503a7c37e8dd31edc256aae10d724852054fe7
Douglas Sjoquist <dwsjoquist@sunetos.com>
2015-02-25 14:23:36 -0500

    Initial Commit

---
+++ SwiftPromises/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPromises
+//
+//  Created by Douglas Sjoquist on 2/25/15.
+//  Copyright (c) 2015 Ivy Gulch LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
