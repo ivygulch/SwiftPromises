4eb0695d885ed44cf7f988bc6415358204a77e3a
Douglas Sjoquist <dwsjoquist@sunetos.com>
2015-02-26 16:56:15 -0500

    modify documentation comments

--- source/Promise.swift
+++ source/Promise.swift
@@ -30,24 +30,24 @@ class Promise {
     }
 
     /**
-    Initializes a new pending promise
-
-    :returns: A pending promise with no chained promises
+    * Initializes a new pending promise
+    *
+    * :returns: A pending promise with no chained promises
     */
     init() {
         state = .Pending([])
     }
 
     /**
-    Initializes a new promise with either an error or a value (which may be nil).
-
-    If the argument is an error, then the state is set to .Rejected otherwise it is set to .Fulfilled
-    In either case, the promise state (by definition) is then immutable.
-
-    This can be useful when an async user code process needs to return a promise, but already
-    has the result (such as with a completed network request, database query, etc.)
-
-    :returns: A rejected or fulfilled promise with no chained promises
+    * Initializes a new promise with either an error or a value (which may be nil).
+    *
+    * If the argument is an error, then the state is set to .Rejected otherwise it is set to .Fulfilled
+    * In either case, the promise state (by definition) is then immutable.
+    *
+    * This can be useful when an async user code process needs to return a promise, but already
+    * has the result (such as with a completed network request, database query, etc.)
+    *
+    * :returns: A rejected or fulfilled promise with no chained promises
     */
     init(_ value:AnyObject?) {
         if let error = value as? NSError {
@@ -58,7 +58,7 @@ class Promise {
     }
 
     /**
-    Read-only property that is true if the promise is still pending
+    * Read-only property that is true if the promise is still pending
     */
     var isPending: Bool {
         get {
@@ -76,7 +76,7 @@ class Promise {
     }
 
     /**
-    Read-only property that is true if the promise has been fulfilled
+    * Read-only property that is true if the promise has been fulfilled
     */
     var isFulfilled: Bool {
         get {
@@ -94,7 +94,7 @@ class Promise {
     }
 
     /**
-    Read-only property that is true if the promise has been rejected
+    * Read-only property that is true if the promise has been rejected
     */
     var isRejected: Bool {
         get {
@@ -112,7 +112,7 @@ class Promise {
     }
 
     /**
-    Read-only property that is the fulfilled value if the promise has been fulfilled, nil otherwise
+    * Read-only property that is the fulfilled value if the promise has been fulfilled, nil otherwise
     */
     var value: AnyObject? {
         var result:AnyObject? = nil
@@ -128,7 +128,7 @@ class Promise {
     }
 
     /**
-    Read-only property that is the rejection error if the promise has been rejected, nil otherwise
+    * Read-only property that is the rejection error if the promise has been rejected, nil otherwise
     */
     var error: NSError? {
         var result:NSError? = nil
@@ -144,11 +144,11 @@ class Promise {
     }
 
     /**
-    If the promise is pending, then change its state to fulfilled using the supplied value
-    and notify any chained promises that it has been fulfilled.  If the promise is in any other
-    state, no changes are made and any chained promises are ignored.
-
-    :param: the fulfilled value to use for the promise
+    * If the promise is pending, then change its state to fulfilled using the supplied value
+    * and notify any chained promises that it has been fulfilled.  If the promise is in any other
+    * state, no changes are made and any chained promises are ignored.
+    *
+    * :param: the fulfilled value to use for the promise
     */
     func fulfill(value: AnyObject?) {
         var promiseActionsToFulfill:[PromiseAction] = []
@@ -167,11 +167,11 @@ class Promise {
     }
 
     /**
-    If the promise is pending, then change its state to rejected using the supplied error
-    and notify any chained promises that it has been rejected.  If the promise is in any other
-    state, no changes are made and any chained promises are ignored.
-
-    :param: the rejection error to use for the promise
+    * If the promise is pending, then change its state to rejected using the supplied error
+    * and notify any chained promises that it has been rejected.  If the promise is in any other
+    * state, no changes are made and any chained promises are ignored.
+    *
+    * :param: the rejection error to use for the promise
     */
     func reject(error: NSError) {
         var promiseActionsToReject:[PromiseAction] = []
@@ -190,31 +190,31 @@ class Promise {
     }
 
     /**
-    'fulfill' and 'reject' closures may be added to a promise at any time. If the promise is
-    eventually fulfilled, the fulfill closure will be called one time, and the reject closure
-    will never be called. If the promise is eventually rejected, the reject closure will be
-    called one time, and the fulfill closure will never be called.  If the promise remains in
-    a pending state, neither closure will ever be called.
-
-    This method may be called as many times as needed, and the appropriate closures will be
-    called in the order they were added via the then method.
-
-    If the promise is pending, then they will be added to the list of closures to be processed
-    once  the promise is fulfilled or rejected in the future.
-
-    If the promise is already fulfilled, then the fulfill closure will be called immediately
-
-    If the promise is already rejected, then if the reject closure exists, it will be called immediately
-
-    :param: fulfill closure to call when the promise is fulfilled
-    It can return:
-    an NSError: it will cause any dependent promises to be rejected with this error
-    a Promise: it will be chained to this instance
-    any other value including nil: it will cause any dependent promises to be fulfilled with this value
-
-    :param: optional rejection closure to call when the promise is rejected
-
-    :returns: a new instance of a promise to which application code can add dependent promises (e.g. chaining)
+    * 'fulfill' and 'reject' closures may be added to a promise at any time. If the promise is
+    * eventually fulfilled, the fulfill closure will be called one time, and the reject closure
+    * will never be called. If the promise is eventually rejected, the reject closure will be
+    * called one time, and the fulfill closure will never be called.  If the promise remains in
+    * a pending state, neither closure will ever be called.
+    *
+    * This method may be called as many times as needed, and the appropriate closures will be
+    * called in the order they were added via the then method.
+    *
+    * If the promise is pending, then they will be added to the list of closures to be processed
+    * once  the promise is fulfilled or rejected in the future.
+    *
+    * If the promise is already fulfilled, then the fulfill closure will be called immediately
+    *
+    * If the promise is already rejected, then if the reject closure exists, it will be called immediately
+    *
+    * :param: fulfill closure to call when the promise is fulfilled
+    *   It can return:
+    *       an NSError: it will cause any dependent promises to be rejected with this error
+    *       a Promise: it will be chained to this instance
+    *       any other value including nil: it will cause any dependent promises to be fulfilled with this value
+    *
+    * :param: optional rejection closure to call when the promise is rejected
+    *
+    * :returns: a new instance of a promise to which application code can add dependent promises (e.g. chaining)
     */
     func then(fulfill: kPromiseFulfillClosure, reject: kPromiseRejectClosure? = nil) -> Promise {
         let result = Promise()
