86ceb1ab7107b5aa89fc4998e84cc922b053be45
Douglas Sjoquist <dwsjoquist@sunetos.com>
2015-11-18 19:26:45 -0500

    add documentation

--- SwiftPromises/source/Promise.swift
+++ SwiftPromises/source/Promise.swift
@@ -8,39 +8,48 @@
 
 import Foundation
 
-public enum PromiseClosureResult<T> {
-    case Error(ErrorType)
-    case Value(T?)
-    case Pending(Promise<T>)
-}
+// MARK: - Promise
 
 /**
-* A Swift version of the Promise pattern popular in the javascript world
-*
-* "A promise represents the eventual result of an asynchronous operation.
-* The primary way of interacting with a promise is through its then method, 
-* which registers callbacks to receive either a promise’s eventual value or 
-* the reason why the promise cannot be fulfilled."  (https://promisesaplus.com) 
-*
-* "The point of promises is to give us back functional composition and error
-* bubbling in the async world. They do this by saying that your functions 
-* should return a promise, which can do one of two things:
-*  - Become fulfilled by a value
-*  - Become rejected with an error" (https://gist.github.com/domenic/3889970)
-*
+A Swift version of the Promise pattern popular in the javascript world
+
+"A promise represents the eventual result of an asynchronous operation.
+The primary way of interacting with a promise is through its then method,
+which registers callbacks to receive either a promise’s eventual value or
+the reason why the promise cannot be fulfilled."  (https://promisesaplus.com)
+
+"The point of promises is to give us back functional composition and error
+bubbling in the async world. They do this by saying that your functions
+should return a promise, which can do one of two things:
+
+*  Become fulfilled by a value
+*  Become rejected with an error" (https://gist.github.com/domenic/3889970)
+
 */
 public class Promise<T> : NSObject {
 
     // MARK: - Interface
 
+    /**
+    - Returns: an immutable, fulfilled promise using the supplied value
+    */
     public class func valueAsPromise(value: T?) -> Promise<T> {
         return Promise(value)
     }
 
+    /**
+     - Returns: an immutable, rejected promise using the supplied error
+     */
     public class func valueAsPromise(value: ErrorType) -> Promise<T> {
         return Promise(value)
     }
 
+    /**
+     - Parameter promises: list of promises that act as dependencies
+
+     - Returns: a dependent promise that is fulfilled when all the supplied promises
+     are fulfilled or rejected when one or more are rejected
+     */
     public class func all(promises:[Promise<T>]) -> Promise<[Promise<T>]> {
         let result:Promise<[Promise<T>]> = Promise<[Promise<T>]>()
         var completedPromiseCount = 0
@@ -69,45 +78,35 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * Initializes a new pending promise
-    *
-    * - returns: A pending promise with no chained promises
-    */
+     Creates a new pending promise with no chained promises
+     */
     public override init() {
         state = .Pending([])
     }
 
     /**
-     * Initializes a new promise with either a value (which may be nil).
-     *
-     * The state is set to .Fulfilled which by definition is then immutable.
-     *
-     * This can be useful when an async user code process needs to return a promise, but already
-     * has the result (such as with a completed network request, database query, etc.)
-     *
-     * - returns: A fulfilled promise with no chained promises
+     Creates a new immutable fulfilled promise with an optional type-safe value and no chained promises
+
+     This can be useful when an async user code process needs to return a promise, but already
+     has the result (such as with a completed network request, database query, etc.)
      */
     public init(_ value:T?) {
         state = .Fulfilled(value)
     }
 
     /**
-     * Initializes a new promise with an error
-     *
-     * The state is set to .Rejected which by definition is then immutable.
-     *
-     * This can be useful when an async user code process needs to return a promise, but already
-     * has an error result (such as with a completed network request, database query, etc.)
-     *
-     * - returns: A rejected promise with no chained promises
+     Creates a new immutable rejected promise with an error and no chained promises
+
+     This can be useful when an async user code process needs to return a promise, but already
+     has an error result (such as with a completed network request, database query, etc.)
      */
     public init(_ error:ErrorType) {
         state = .Rejected(error)
     }
 
     /**
-    * Read-only property that is true if the promise is still pending
-    */
+     Read-only property that is true if the promise is still pending
+     */
     public var isPending: Bool {
         get {
             var result = false
@@ -124,8 +123,8 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * Read-only property that is true if the promise has been fulfilled
-    */
+     Read-only property that is true if the promise has been fulfilled
+     */
     public var isFulfilled: Bool {
         get {
             var result = false
@@ -142,8 +141,8 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * Read-only property that is true if the promise has been rejected
-    */
+     Read-only property that is true if the promise has been rejected
+     */
     public var isRejected: Bool {
         get {
             var result = false
@@ -160,8 +159,8 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * Read-only property that is the fulfilled value if the promise has been fulfilled, nil otherwise
-    */
+     Read-only property that is the fulfilled value if the promise has been fulfilled, nil otherwise
+     */
     public var value: T? {
         var result:T?
         stateSynchronizer.synchronize {
@@ -176,8 +175,8 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * Read-only property that is the rejection error if the promise has been rejected, nil otherwise
-    */
+     Read-only property that is the rejection error if the promise has been rejected, nil otherwise
+     */
     public var error: ErrorType? {
         var result:ErrorType?
         stateSynchronizer.synchronize {
@@ -192,12 +191,12 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * If the promise is pending, then change its state to fulfilled using the supplied value
-    * and notify any chained promises that it has been fulfilled.  If the promise is in any other
-    * state, no changes are made and any chained promises are ignored.
-    *
-    * - parameter the: fulfilled value to use for the promise
-    */
+     If the promise is pending, then change its state to fulfilled using the supplied value
+     and notify any chained promises that it has been fulfilled.  If the promise is in any other
+     state, no changes are made and any chained promises are ignored.
+
+     - Parameter value: optional fulfilled value to use for the promise
+     */
     public func fulfill(value: T?) {
         var promiseActionsToFulfill:[PromiseAction<T>] = []
         stateSynchronizer.synchronize {
@@ -215,12 +214,12 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * If the promise is pending, then change its state to rejected using the supplied error
-    * and notify any chained promises that it has been rejected.  If the promise is in any other
-    * state, no changes are made and any chained promises are ignored.
-    *
-    * - parameter the: rejection error to use for the promise
-    */
+     If the promise is pending, then change its state to rejected using the supplied error
+     and notify any chained promises that it has been rejected.  If the promise is in any other
+     state, no changes are made and any chained promises are ignored.
+
+     - Parameter error: rejection error to use for the promise
+     */
     public func reject(error: ErrorType) {
         var promiseActionsToReject:[PromiseAction<T>] = []
         stateSynchronizer.synchronize {
@@ -238,32 +237,37 @@ public class Promise<T> : NSObject {
     }
 
     /**
-    * 'fulfill' and 'reject' closures may be added to a promise at any time. If the promise is
-    * eventually fulfilled, the fulfill closure will be called one time, and the reject closure
-    * will never be called. If the promise is eventually rejected, the reject closure will be
-    * called one time, and the fulfill closure will never be called.  If the promise remains in
-    * a pending state, neither closure will ever be called.
-    *
-    * This method may be called as many times as needed, and the appropriate closures will be
-    * called in the order they were added via the then method.
-    *
-    * If the promise is pending, then they will be added to the list of closures to be processed
-    * once  the promise is fulfilled or rejected in the future.
-    *
-    * If the promise is already fulfilled, then the fulfill closure will be called immediately
-    *
-    * If the promise is already rejected, then if the reject closure exists, it will be called immediately
-    *
-    * - parameter fulfill: closure to call when the promise is fulfilled
-    *   It can return:
-    *       an ErrorType: it will cause any dependent promises to be rejected with this error
-    *       a Promise: it will be chained to this instance
-    *       any other value including nil: it will cause any dependent promises to be fulfilled with this value
-    *
-    * - parameter optional: rejection closure to call when the promise is rejected
-    *
-    * - returns: a new instance of a promise to which application code can add dependent promises (e.g. chaining)
-    */
+     This method lets your code add dependent results to an existing promise.
+
+     'fulfill' and 'reject' closures may be added to a promise at any time. If the promise is
+     eventually fulfilled, the fulfill closure will be called one time, and the reject closure
+     will never be called. If the promise is eventually rejected, the reject closure will be
+     called one time, and the fulfill closure will never be called.  If the promise remains in
+     a pending state, neither closure will ever be called.
+
+     This method may be called as many times as needed, and the appropriate closures will be
+     called in the order they were added via the then method.
+
+     If the promise is pending, then they will be added to the list of closures to be processed
+     once the promise is fulfilled or rejected in the future.
+
+     If the promise is already fulfilled, then the fulfill closure will be called immediately
+
+     If the promise is already rejected, then if the reject closure exists, it will be called immediately
+
+     - Parameter fulfill: closure to call when the promise is fulfilled that returns an instance of PromiseClosureResult
+
+     - Parameter reject: optional rejection closure to call when the promise is rejected that returns an instance of PromiseClosureResult
+
+     If the reject closure does not exist, it will simply pass the .Error(errorType) value down the promise chain
+
+     If it does exist, it returns an instance of PromiseClosureResult, just as the fulfill closure does.
+
+     (Note that this means that either closure can redirect dependent promises in the chain, which can
+     allow a reject closure to to recover by supplying a valid value so subsequent promises can continue)
+
+     - Returns: a new instance of a promise to which application code can add dependent promises (e.g. chaining)
+     */
     public func then(fulfill: ((T?) -> PromiseClosureResult<T>), reject: ((ErrorType) -> PromiseClosureResult<T>)?) -> Promise<T> {
         let result:Promise<T> = Promise()
         let promiseAction = PromiseAction(result, fulfill, reject)
@@ -281,17 +285,65 @@ public class Promise<T> : NSObject {
         return result
     }
 
-    // Need separate method definition since Objective-C does not recognizer default parameters
+    /**
+     A convenience method with a nil reject closure.
+     Since Objective-C does not recognize default parameters, this provides a work-around.
+     */
     public func then(fulfill: ((T?) -> PromiseClosureResult<T>)) -> Promise<T> {
         return then(fulfill, reject: nil)
     }
 
-    // MARK: - implementation
+    // MARK: - private values
 
     private var state: PromiseState<T> = .Pending([])
     private let stateSynchronizer = Synchronizer()
 
 }
+
+// MARK: -
+// MARK: Public helper enum PromiseClosureResult
+
+/**
+PromiseClosureResult is returned by promise fulfill and reject closures so they can return 
+a type-safe value, an error, or a promise to be used in the promise chain.  This allows 
+either type of closure to pass along a similar value (e.g. type-safe value -> type-safe value) 
+or redirect the dependent, chained promises into an alternative path by returning a type-safe 
+value from a reject closure.
+
+<ul>
+<li>.Value - it will cause any dependent promises to be fulfilled with the associated type-safe value</li>
+<li>.Error - it will cause any dependent promises to be rejected with the associated error value</li>
+<li>.Pending - it will chain this instance's resolution to the associated promise's resolution</li>
+</ul>
+*/
+public enum PromiseClosureResult<T> {
+    /**
+     Associated value is required and is the error that will trigger
+     the reject closure for a promise
+     */
+    case Error(ErrorType)
+    /**
+     Associated value is optional and is the type-safe result that will trigger
+     the fulfill closure for a promise
+     */
+    case Value(T?)
+    /**
+     Associated value is required and is a type-safe promise that the owning promise will be dependent
+     on for a result before the owning promise can be fulfilled or rejected
+     */
+    case Pending(Promise<T>)
+}
+
+// MARK: -
+// MARK: Private helper enum PromiseState
+
+private enum PromiseState<T> {
+    case Pending([PromiseAction<T>])
+    case Fulfilled(T?)
+    case Rejected(ErrorType)
+}
+
+// MARK: Private helper class PromiseAction
 
 private class PromiseAction<T> {
     private let promise: Promise<T>
@@ -303,7 +355,7 @@ private class PromiseAction<T> {
         self.fulfillClosure = fulfillClosure
         self.rejectClosure = rejectClosure
     }
-    
+
 
     func fulfill(value: T?) {
         let result = fulfillClosure(value)
@@ -340,14 +392,6 @@ private class PromiseAction<T> {
             )
         }
     }
-
-
-}
-
-// MARK: private helper definitions
-
-private enum PromiseState<T> {
-    case Pending([PromiseAction<T>])
-    case Fulfilled(T?)
-    case Rejected(ErrorType)
+    
+    
 }
--- SwiftPromises/source/Synchronizer.swift
+++ SwiftPromises/source/Synchronizer.swift
@@ -8,22 +8,56 @@
 
 import Foundation
 
+/**
+A simple helper class that uses dispatch_sync to emulate Objective-C's @synchronize behavior
+in swift classes.
+ 
+Example usage:
+
+```swift
+let synchronizer = Synchronizer()
+
+func functionAToBeSynchronized() {
+    synchronizer.synchronize {
+        // this is the code to be synchronized
+    }
+}
+
+func functionBToBeSynchronized() {
+    synchronizer.synchronize {
+        // this is the code to be synchronized
+    }
+}
+```
+*/
 public class Synchronizer : NSObject {
     private let queue:dispatch_queue_t
 
+    /**
+     Creates a new synchronizer that uses a newly created, custom queue with a random name
+     */
     public override init() {
         let uuid = NSUUID().UUIDString
         self.queue = dispatch_queue_create("Sync.\(uuid)",nil)
     }
 
+    /**
+     Creates a new synchronizer that uses a newly created, custom queue with a given name
+     */
     public init(queueName:String) {
         self.queue = dispatch_queue_create(queueName,nil)
     }
 
+    /**
+     Creates a new synchronizer that uses an existing dispatch queue
+     */
     public init(queue:dispatch_queue_t) {
         self.queue = queue
     }
 
+    /**
+     - Parameter closure: the closure to be synchronized
+     */
     public func synchronize(closure:()->Void) {
         dispatch_sync(queue, {
             closure()
